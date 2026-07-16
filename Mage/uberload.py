7ec9d0831051ff271576b75e1f4057741ddd62d4
Carlton Subrata <143212299+carlton1305@users.noreply.github.com>
2026-07-16 01:24:52 -0700

    Add files via upload

---
+++ Mage/uberload.py
@@ -0,0 +1,25 @@
+import io
+import pandas as pd
+import requests
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data_from_api(*args, **kwargs):
+    """
+    Template for loading data from API
+    """
+    url = 'https://storage.googleapis.com/uber-taxi-data-engineering-project/yellow_tripdata_2026-01.parquet'
+    df = pd.read_parquet(url).head(100000) #limit the data since it is too heavy
+    return df
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
